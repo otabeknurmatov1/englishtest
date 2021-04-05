9376971efa534667bf0b3d54dc2b894ca1ca8827
Otabek Nurmatov <73683844+otabeknurmatov1@users.noreply.github.com>
2021-04-05 10:23:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -150,12 +150,12 @@ function showResult(){
     quiz_box.classList.remove("activeQuiz"); //hide quiz box
     result_box.classList.add("activeResult"); //show result box
     const scoreText = result_box.querySelector(".score_text");
-    if (userScore > 15){ // if user scored more than 3
+    if (userScore > 12){ // if user scored more than 3
         //creating a new span tag and passing the user score number and total question number
         let scoreTag = '<span>wow congrats! 🎉, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
     }
-    else if(userScore > 10){ // if user scored more than 1
+    else if(userScore > 7){ // if user scored more than 1
         let scoreTag = '<span>Nice work 😎, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
     }
